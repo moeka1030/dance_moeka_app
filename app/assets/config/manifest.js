85fdd820229ebe16fc934505f00cc2f08e68a678
Nagatomi Moeka <moeka@NagatominoMacBook-Pro.local>
2025-03-06 16:05:38 +0900

    レイアウトレイアウト途中途中

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -3,6 +3,9 @@
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link posts/show.css
+//= link posts/index.css
+//= link static_pages/home.css
+//= link users/_profile_edit.css
 //= link_tree ../stylesheets
 //= link_directory ../stylesheets/users .css
 
